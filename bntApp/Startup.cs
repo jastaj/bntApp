15413a4e401ebde9f7ce646456014a93600fe599
Aman Preet <kauramn.167@gmail.com>
2023-08-30 17:06:10 +0530

    Add project files.

---
+++ bntApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bntApp.Startup))]
+namespace bntApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
